667e7dfdc1cf3d834206f28c698eef377a1a39ae
mamad-love <mamad.love.mail@gmail.com>
2020-11-18 15:34:15 +0330

    redis loader modified

--- redis_loader.py
+++ redis_loader.py
@@ -2,13 +2,13 @@ from sys import argv
 import redis
 from re import search
 
-r = redis.Redis()
-r2 = redis.Redis(db=1)
+userid = redis.Redis()
+username = redis.Redis(db=1)
 
 with open(argv[1]) as users:
     for user in users:
         data = search('''{'id': (.*)?, 'username': '(.*)?', 'phone': '(.*)?'}''', user)
-        r.set(data[1], data[3])
-        r2.set(data[2], data[3])
+        userid.set(data[1], data[3]+';'+data[2])
+        username.set(data[2], data[3]+';'+data[1])
         print(data[3])
 input('\n\nLoaded: '+argv[1])
