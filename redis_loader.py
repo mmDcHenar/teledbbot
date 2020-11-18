c92a72679482fcc04ba82a444c47a4891fb3bf84
mamad-love <mamad.love.mail@gmail.com>
2020-11-18 17:43:31 +0330

    bug fix "for blank line in txt file"

--- redis_loader.py
+++ redis_loader.py
@@ -8,7 +8,8 @@ username = redis.Redis(db=1)
 with open(argv[1]) as users:
     for user in users:
         data = search('''{'id': (.*)?, 'username': '(.*)?', 'phone': '(.*)?'}''', user)
-        userid.set(data[1], data[3]+';'+data[2])
-        username.set(data[2], data[3]+';'+data[1])
-        print(data[3])
+        if data:
+            userid.set(data[1], data[3]+';'+data[2])
+            username.set(data[2], data[3]+';'+data[1])
+            print(data[3])
 input('\n\nLoaded: '+argv[1])
